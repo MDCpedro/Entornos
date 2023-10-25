dd4a96a3d3ea7ef39ced2b1981b88170033e3407
MDCpedro <pedromdc101@gmail.com>
2023-10-25 16:07:54 +0200

    Act 4

--- Python/Act5/Ex_4.py
+++ Python/Act5/Ex_4.py
@@ -3,4 +3,14 @@ pwd = "eureka"
 acierto = False
 
 while contador != 0 and acierto == False:
-    print()
+    pwd = str(input("Escribe la contraseña: "))
+    if pwd != "eureka" and contador > 0:
+        contador = contador-1
+        print("Contraseña incorrecta, intentalo de nuevo.")
+
+    elif pwd == "eureka":
+        acierto = True
+
+    elif contador == 0:
+        print("Intentos agotados.")
+        
