51d467c6c768e157e42d1a781e7f3ab94bdb1d70
MDCpedro <pedromdc101@gmail.com>
2023-12-05 19:57:38 +0100

    Act  Java

--- Java/Actividades/src/ActividadesJava/BuscarNombres.java
+++ Java/Actividades/src/ActividadesJava/BuscarNombres.java
@@ -8,15 +8,19 @@ public class BuscarNombres {
         System.out.println("Escribe el nombre que quieres buscar");
         String nombrebuscar = preguntaNombre.nextLine();
 
-
+        boolean encontrado = false;
         int index = 0;
         while (index < nombres.length) {
             if (nombres[index].equals(nombrebuscar)) {
                 System.out.println("El nombre se encuentra en la posicion "+index);
-                index = 999;
+                encontrado = true;
+                break;
             } else {
                 index++;
             }
+        }
+        if (!encontrado) {
+            System.out.println("No se ha encontrado el nombre");
         }
     }
 }
