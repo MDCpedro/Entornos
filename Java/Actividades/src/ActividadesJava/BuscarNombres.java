64178fb37c0d56c7cb9f8a923bcff366079bae51
MDCpedro <pedromdc101@gmail.com>
2023-12-05 19:48:42 +0100

    Act  Java

---
+++ Java/Actividades/src/ActividadesJava/BuscarNombres.java
@@ -0,0 +1,22 @@
+package ActividadesJava;
+import java.util.Scanner;
+public class BuscarNombres {
+    public static void main(String[] args) {
+        String nombres[] = {"Pedro", "Freddy", "Daniel", "Aina", "Juan", "Irek", "Pablo Motos"};
+
+        Scanner preguntaNombre =  new Scanner(System.in);
+        System.out.println("Escribe el nombre que quieres buscar");
+        String nombrebuscar = preguntaNombre.nextLine();
+
+
+        int index = 0;
+        while (index < nombres.length) {
+            if (nombres[index].equals(nombrebuscar)) {
+                System.out.println("El nombre se encuentra en la posicion "+index);
+                index = 999;
+            } else {
+                index++;
+            }
+        }
+    }
+}
