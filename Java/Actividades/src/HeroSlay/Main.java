67f645d9ffdd5c49b6468ccc509d273c0dbec0c6
MDCpedro <pedromdc101@gmail.com>
2024-01-25 19:17:38 +0100

    Act HeroSlay

---
+++ Java/Actividades/src/HeroSlay/Main.java
@@ -0,0 +1,4 @@
+package HeroSlay;
+
+public class Main  {
+}
---
+++ Java/Actividades/src/HeroSlay/Personaje.java
@@ -0,0 +1,7 @@
+package HeroSlay;
+
+public class Personaje {
+    private String Nombre;
+    private int Vida;
+    private String Mazo;
+}
