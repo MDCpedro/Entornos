eb848ea6dc24e92da3757234ade740bc29154595
MDCpedro <pedromdc101@gmail.com>
2024-01-29 19:08:00 +0100

    Act java heroes

--- Java/Actividades/src/HeroSlay/Main.java
+++ Java/Actividades/src/HeroSlay/Main.java
@@ -1,4 +1,45 @@
 package HeroSlay;
 
-public class Main  {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Carta carta1 = new Carta("Espadazo", "ATAQUE", 3);
+        Carta carta2 = new Carta("PocionCurativa", "CURACION", 2);
+
+
+        List<Carta> mazo = new ArrayList<>();
+
+        mazo.add(carta1);
+        mazo.add(carta2);
+
+        Personaje heroe = new Personaje("Yuuji", 10, mazo);
+        Personaje villano = new Personaje("Sukuna", 10, mazo);
+
+        System.out.println("Pelea entre" + heroe.getNombre());
+
+        pelea(heroe, villano, mazo);
+
+
+
+    }
+
+    public static void pelea(Personaje heroe, Personaje villano, List Mazo) {
+        Random random = new Random();
+        boolean HeroeEmpieza = random.nextBoolean();
+        int Seleccion = random.nextInt(0, 2);
+
+        System.out.println("Pelea entre " + heroe.getNombre() + " y " + villano.getNombre() +"!!!");
+        System.out.println(heroe.getNombre()+ ", Vida: " +heroe.getVida() +", Mazo: " +heroe.getMazo());
+
+
+
+
+
+    }
+
+
 }
--- Java/Actividades/src/HeroSlay/Personaje.java
+++ Java/Actividades/src/HeroSlay/Personaje.java
@@ -1,7 +1,27 @@
 package HeroSlay;
 
+import java.util.List;
+
 public class Personaje {
-    private String Nombre;
-    private int Vida;
-    private String Mazo;
+    private String nombre;
+    private int vida;
+    private List<Carta> mazo;
+
+    public Personaje(String nombre, int vida, List<Carta> mazo) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.mazo = mazo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public List<Carta> getMazo() {
+        return mazo;
+    }
 }
